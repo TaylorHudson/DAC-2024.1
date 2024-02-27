dde6132561dfdd5c2be5e16309ab1645f96841af
TaylorHudson <taylorhudsonsantos@gmail.com>
2024-02-27 19:08:10 -0300

    Adding User entity

---
+++ src/main/java/br/edu/ifpb/dac/domain/User.java
@@ -0,0 +1,103 @@
+package br.edu.ifpb.dac.domain;
+
+import br.edu.ifpb.dac.domain.enumeration.UserType;
+import jakarta.persistence.*;
+
+@Table(name = "USER_TB")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(unique = true, nullable = false)
+    private Long id;
+    @Column(nullable = false)
+    private String firstName;
+    @Column(nullable = false)
+    private String lastName;
+    @Column(unique = true, nullable = false)
+    private String email;
+    @Column(nullable = false)
+    private String password;
+    @Column(unique = true, nullable = false)
+    private String document;
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private UserType type;
+
+    public User(Long id, String firstName, String lastName, String email, String password, String document, UserType type) {
+        this.id = id;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
+        this.password = password;
+        this.document = document;
+        this.type = type;
+    }
+
+    public User(String firstName, String lastName, String email, String password, String document, UserType type) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
+        this.password = password;
+        this.document = document;
+        this.type = type;
+    }
+
+    public User() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getDocument() {
+        return document;
+    }
+
+    public void setDocument(String document) {
+        this.document = document;
+    }
+
+    public UserType getType() {
+        return type;
+    }
+
+    public void setType(UserType type) {
+        this.type = type;
+    }
+}
