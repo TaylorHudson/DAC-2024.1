cfe3bc22fe008965c4f782c49d8733f368126292
TaylorHudson <taylorhudsonsantos@gmail.com>
2024-02-28 19:41:39 -0300

    Fix: Adds code attribute to enum UserType

--- src/main/java/br/edu/ifpb/dac/domain/enumeration/UserType.java
+++ src/main/java/br/edu/ifpb/dac/domain/enumeration/UserType.java
@@ -1,6 +1,33 @@
 package br.edu.ifpb.dac.domain.enumeration;
 
+import java.util.Arrays;
+
 public enum UserType {
-    OPERATOR,
-    CUSTOMER
+
+    CUSTOMER(1),
+    OPERATOR(2);
+
+    public int code;
+
+    UserType(int code) {
+        this.code = code;
+    }
+
+    public static UserType findByCode(int code) {
+        var types = UserType.values();
+        for (UserType type: types) {
+            if (type.code == code)
+                return type;
+        }
+        throw new RuntimeException("Código inválido: ".concat(String.valueOf(code)));
+    }
+
+    public void setCode(int code) {
+        this.code = code;
+    }
+
+    public int getCode() {
+        return code;
+    }
+
 }
