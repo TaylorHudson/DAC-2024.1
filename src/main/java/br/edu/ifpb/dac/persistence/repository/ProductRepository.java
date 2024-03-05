01da2998d6187dbe7708ce71616634371b5bac8a
TaylorHudson <taylorhudsonsantos@gmail.com>
2024-03-05 15:20:55 -0300

    Adding Product repository

---
+++ src/main/java/br/edu/ifpb/dac/persistence/repository/ProductRepository.java
@@ -0,0 +1,8 @@
+package br.edu.ifpb.dac.persistence.repository;
+
+import br.edu.ifpb.dac.domain.Product;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ProductRepository extends JpaRepository<Product, Long> {
+    boolean existsByName(String name);
+}
