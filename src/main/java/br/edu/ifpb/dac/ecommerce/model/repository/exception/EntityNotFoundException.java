ebe9c950eb6906937197964324f14b634851b887
TaylorHudson <taylorhudsonsantos@gmail.com>
2024-03-12 14:04:45 -0300

    Fix: Refactor exceptions messages

--- src/main/java/br/edu/ifpb/dac/ecommerce/model/repository/exception/EntityNotFoundException.java
+++ src/main/java/br/edu/ifpb/dac/ecommerce/model/repository/exception/EntityNotFoundException.java
@@ -1,7 +1,7 @@
 package br.edu.ifpb.dac.ecommerce.model.repository.exception;
 
 public class EntityNotFoundException extends RuntimeException {
-    public EntityNotFoundException(String message) {
-        super(message);
+    public EntityNotFoundException(String field) {
+        super("Entity with the " + field.trim() + " supplied is not found.");
     }
 }
