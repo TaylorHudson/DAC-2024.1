c8204131d10fc3d7be7521c11e34e014817d24b3
TaylorHudson <taylorhudsonsantos@gmail.com>
2024-03-12 13:49:34 -0300

    Fix: Adjust ProductController

--- src/main/java/br/edu/ifpb/dac/ecommerce/presentation/controller/ProductController.java
+++ src/main/java/br/edu/ifpb/dac/ecommerce/presentation/controller/ProductController.java
@@ -1,35 +1,58 @@
 package br.edu.ifpb.dac.ecommerce.presentation.controller;
 
-import br.edu.ifpb.dac.ecommerce.model.entity.Product;
+import br.edu.ifpb.dac.ecommerce.business.mapper.Mapper;
 import br.edu.ifpb.dac.ecommerce.business.service.ProductService;
+import br.edu.ifpb.dac.ecommerce.model.entity.Product;
+import br.edu.ifpb.dac.ecommerce.presentation.dto.ProductRequestDto;
+import br.edu.ifpb.dac.ecommerce.presentation.dto.ProductResponseDto;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
-import org.springframework.stereotype.Controller;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
-@Controller
+@RestController
+@RequestMapping("/product")
 @RequiredArgsConstructor
 public class ProductController {
 
     private final ProductService productService;
+    private final Mapper<ProductRequestDto, Product> requestToProductMapper;
+    private final Mapper<Product, ProductResponseDto> productToResponseMapper;
 
-    public Product save(Product product) {
-        return productService.save(product);
+    @PostMapping
+    public ResponseEntity<ProductResponseDto> save(@RequestBody @Valid ProductRequestDto requestDto) {
+        Product product = productService.save(requestToProductMapper.map(requestDto));
+        return ResponseEntity.status(HttpStatus.CREATED).body(productToResponseMapper.map(product));
     }
 
-    public List<Product> getProducts() {
-        return productService.getProducts();
+    @GetMapping
+    public ResponseEntity<List<ProductResponseDto>> getProducts() {
+        List<ProductResponseDto> response = productService.getProducts()
+                .stream()
+                .map(productToResponseMapper::map)
+                .toList();
+
+        return ResponseEntity.status(HttpStatus.OK).body(response);
     }
 
-    public Product getProductById(Long id) {
-        return productService.getProductById(id);
+    @GetMapping("/{id}")
+    public ResponseEntity<ProductResponseDto> getProductById(@PathVariable Long id) {
+        ProductResponseDto response = productToResponseMapper.map(productService.getProductById(id));
+        return ResponseEntity.status(HttpStatus.OK).body(response);
     }
 
-    public Product update(Long id, Product updatedProduct) {
-        return productService.update(id, updatedProduct);
+    @PutMapping("/{id}")
+    public ResponseEntity<ProductResponseDto> update(@PathVariable Long id, @RequestBody @Valid ProductRequestDto requestDto) {
+        Product product = productService.update(id, requestToProductMapper.map(requestDto));
+        return ResponseEntity.status(HttpStatus.OK).body(productToResponseMapper.map(product));
     }
 
-    public void delete(Long id) {
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> delete(@PathVariable Long id) {
         productService.delete(id);
+        return ResponseEntity.noContent().build();
     }
 }
