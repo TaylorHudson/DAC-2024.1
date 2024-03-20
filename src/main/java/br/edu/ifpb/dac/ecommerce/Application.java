3931fed4532be18591c5f34da51422d09d70ae18
TaylorHudson <taylorhudsonsantos@gmail.com>
2024-03-20 11:05:10 -0300

    Adding swagger api definition

--- src/main/java/br/edu/ifpb/dac/ecommerce/Application.java
+++ src/main/java/br/edu/ifpb/dac/ecommerce/Application.java
@@ -1,8 +1,21 @@
 package br.edu.ifpb.dac.ecommerce;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.servers.Server;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@OpenAPIDefinition(
+        info = @Info(
+                title = "Ecommerce API",
+                version = "1.0.0"
+        ),
+        servers = @Server(
+                url = "http://localhost:8080/api",
+                description = "Local Environment"
+        )
+)
 @SpringBootApplication
 public class Application {
 
